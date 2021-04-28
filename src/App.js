c6ff49b9b70ca99397120fbc9dca8e3cb304faea
theafolayan <theafolayan@gmail.com>
2021-04-28 16:56:08 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function Test() {
+  return (
+    <div>
+      <h4> Hello Brother!</h4>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Test></Test>
+    </div>
+  );
+}
+
+export default App;
