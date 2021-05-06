5862c9d9a7308c9e67f3a663502e29959e334352
theafolayan <theafolayan@gmail.com>
2021-05-06 17:34:42 +0100

    fixed import issue

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-// import logo from "./logo.svg";
-import "./App.css";
+/** @jsxImportSource @emotion/react */
 import "twin.macro";
-import tw from "twin.macro";
-// const Button = tw.button`border hover:border-black bg-green-200 p-3`;
-const PrimaryButton = tw.button`bg-blue-800 text-white px-6 py-2 m-6 rounded-md hover:bg-blue-600`;
+
+function DemoTestimonial() {
+  return (
+    <div tw="md:flex m-12 bg-gray-100 rounded-xl p-8 md:p-0">
+      <img
+        tw="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+        src="https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg"
+        alt=""
+        width="384"
+        height="512"
+      />
+      <div tw="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <blockquote>
+          <p tw="text-lg font-semibold">
+            “Tailwind CSS is the only framework that I've seen scale on large
+            teams. It’s easy to customize, adapts to any design, and the build
+            size is tiny.”
+          </p>
+        </blockquote>
+        <div tw="font-medium">
+          <div tw="text-blue-600">Sarah Dayan</div>
+          <div tw="text-gray-500">Staff Engineer, Algolia</div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <PrimaryButton onClick={console.log("clicked")}> Click me</PrimaryButton>
+    <div>
+      <DemoTestimonial />
     </div>
   );
 }
